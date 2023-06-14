7779a78cd11921e3b390eb80494a704909cffeec
kaushal <kaushal8650@gmail.com>
2023-06-14 14:16:14 +0530

    14 june 2023

--- src/main/java/com/kareem/kaushal/controller/HomeController.java
+++ src/main/java/com/kareem/kaushal/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.kareem.kaushal.controller;
 
 import com.kareem.kaushal.model.AroojRequest;
+import com.kareem.kaushal.model.Book;
 import com.kareem.kaushal.model.PaytmModelResponse;
 
+import com.kareem.kaushal.repo.ReadingListRepository;
 import com.kareem.kaushal.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class HomeController {
+
+
+    @Autowired
+    public ReadingListRepository readingListRepository;
 
 //    @Autowired(required = false)
 //    ReadingListController readingListController;
@@ -22,6 +31,25 @@ public class HomeController {
 
         return "My name is "+ kaushal;
     }
+
+    @PostMapping("/save")
+    public String saveIntoBook(@RequestBody Book book){
+        readingListRepository.save(book);
+        return "your book is saved in db!!";
+    }
+    @GetMapping("/fetch/{id}")
+    public Book fetchBookObjectFromDb(@PathVariable Long id) {
+        Optional<Book> fetchedDataFromDb = null;
+        Book dbObject =null;
+        try {
+            fetchedDataFromDb = readingListRepository.findById(id);
+            dbObject = fetchedDataFromDb.get();
+        } catch (Exception e) {
+            System.out.println("exception is " + e.getMessage());
+        }
+        return dbObject;
+    }
+
     @GetMapping("/arooj")
     public String testMethod(){
         AroojRequest a = new AroojRequest();
